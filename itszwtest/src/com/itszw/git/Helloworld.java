a26f477a859b0b7e4bbe18ceedc0b5f3dae49f68
KiritoSzw <szwapp@sina.com>
2021-10-31 16:15:40 -0400

    second add

--- itszwtest/src/com/itszw/git/Helloworld.java
+++ itszwtest/src/com/itszw/git/Helloworld.java
@@ -3,5 +3,6 @@ package com.itszw.git;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World" );
+        System.out.println("Hello World" );
     }
 }
